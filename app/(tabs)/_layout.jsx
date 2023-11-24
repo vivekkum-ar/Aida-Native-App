2d1f12e73f92ee88d8d5c0f86861709f2344d585
vivekkum-ar <vivekkr9119@gmail.com>
2023-11-24 10:38:58 +0530

    Added screens

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Profile = () => {
+const TabsLayout = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>TabsLayout</Text>
     </View>
   )
 }
 
-export default Profile
+export default TabsLayout
 
 const styles = StyleSheet.create({})
--- app/index.js
+++ app/index.js
@@ -1,11 +1,18 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+
 export default function Page() {
+  
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl">AIDA</Text>
       <Text>This is the first page of your app.</Text>
-      <Link href={"/profile"} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Profile</Link>
+      <Link
+        href={"/profile"}
+        className="text-black font-pblack bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Profile
+      </Link>
     </View>
   );
 }
