8e866e21c17386b00238b09a056998f1b48c083e
vivekkum-ar <vivekkr9119@gmail.com>
2023-11-27 12:55:29 +0530

    Passing searchterm from homepage to [query] works2

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
   const { query } = useLocalSearchParams();
   return (
-    <View>
+    <SafeAreaView className="bg-primary h-full">
       <Text className="text-3xl text-white">Search</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
